873f06d1302c874c30a600863d74d755cf637b69
blobii <realblobii@proton.me>
2026-03-22 11:22:56 +0800

    feat: added a nice text box and stretched the text input box

--- main.c
+++ main.c
@@ -42,8 +42,8 @@ int main()
                 if (highlight > taskCount) highlight = 1;
                 break;
             case '+':                  
-		newTask(); 
-		break;
+                newTask(); 
+                break;
             case 'q':
                 goto cleanup;
         }
@@ -75,19 +75,29 @@ void printTasks(int highlight)
 
 void newTask()
 {
-    int h = 7, w = 40;
+    int h = 7, w = 75;
     int y = (LINES - h) / 2;
     int x = (COLS - w) / 2;
 
     WINDOW *popup = newwin(h, w, y, x);
     box(popup, 0, 0);
     mvwprintw(popup, 1, 2, "Enter new task:");
+   
+    int tbw = w-5;
+    char buf[tbw + 1];
+    memset(buf, '-', tbw);
+    buf[tbw] = '\0';
+
+    mvwprintw(popup, 2, 2, "%s", buf);
+    mvwprintw(popup, 3, 2, "|");
+    mvwprintw(popup, 3, w-4, "|");
+    mvwprintw(popup, 4, 2, "%s", buf);
     mvwprintw(popup, h-2, 2, "Press Enter to save");
     wrefresh(popup);
 
     echo();
-    char buffer[100];
-    mvwgetnstr(popup, 3, 2, buffer, sizeof(buffer)-1); // user types here
+    char buffer[tbw-3];
+    mvwgetnstr(popup, 3, 4, buffer, tbw-4); // user types here
     noecho();
 
 
